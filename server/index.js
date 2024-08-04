92d196d002af5b026d7b85b467adca348cdcadfc
VigneshM454 <120500685+VigneshM454@users.noreply.github.com>
2024-08-04 19:22:57 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -28,6 +28,7 @@ app.use(cors(
 ))
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended:true}))
+app.set('trust proxy',true)
 app.use(SessionDemo({
     secret:process.env.SESSIONSECRET,
     resave:false,
